63ee4b9505507590353100222c9966b26918dd88
maja <eaamkly@students.eaaa.dk>
2023-11-01 10:29:09 +0100

    nav bar

--- cheersMate/src/assets/Components/Pages/Home.jsx
+++ cheersMate/src/assets/Components/Pages/Home.jsx
@@ -1,3 +1,20 @@
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="pickChoose">
+      <div className="pickHeading">
+        <h1>
+          Pick & Choose
+          <span className="smilyFace">
+            <img src="src\assets\Icons\☺.svg" />
+          </span>
+        </h1>
+      </div>
+      <div className="pickContent">
+        <div className="cardFav topCards">
+          <img src="src\assets\Icons\heart.svg" />
+          <h3>Favorite</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
