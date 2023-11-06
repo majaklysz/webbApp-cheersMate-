752108f811ec3d61fed1021bf54c7f4e83d117b8
maja <eaamkly@students.eaaa.dk>
2023-11-06 13:11:52 +0100

    filters//tags
    hope it works :)

--- cheersMate/src/assets/Components/Games.jsx
+++ cheersMate/src/assets/Components/Games.jsx
@@ -1,9 +1,38 @@
 import { useEffect, useState } from "react";
-
 import GameCard from "./GameCard";
 
 export default function Games() {
   const [games, setGames] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [sortBy, setSortBy] = useState("createdAt");
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [dropdown, setDropdown] = useState(false);
+  const allTags = [
+    "indoor",
+    "outdoor",
+    "cards",
+    "dices",
+    "team",
+    "pen&paper",
+    "non-drinkers",
+    "recomended",
+  ];
+
+  const handleDorpdown = () => {
+    setDropdown(!dropdown);
+  };
+
+  // Define handleTagSelection function
+  const handleTagSelection = (e) => {
+    const tag = e.target.value;
+    if (e.target.checked) {
+      setSelectedTags([...selectedTags, tag]);
+    } else {
+      setSelectedTags(
+        selectedTags.filter((selectedTag) => selectedTag !== tag)
+      );
+    }
+  };
 
   useEffect(() => {
     async function getGames() {
@@ -14,15 +43,11 @@ export default function Games() {
       const gamesArray = Object.keys(data).map((key) => ({
         id: key,
         ...data[key],
-      })); // from object to array
-
+      }));
       setGames(gamesArray);
     }
     getGames();
   }, []);
-
-  const [searchValue, setSearchValue] = useState("");
-  const [sortBy, setSortBy] = useState("createdAt");
 
   let gamesToDisplay = [...games];
 
@@ -32,12 +57,17 @@ export default function Games() {
     );
   }
 
+  if (selectedTags.length > 0) {
+    gamesToDisplay = gamesToDisplay.filter((game) =>
+      selectedTags.every((tag) => game.tags.includes(tag))
+    );
+  }
+
   gamesToDisplay.sort((game1, game2) => {
-    console.log(sortBy);
     if (sortBy === "name") {
-      return game1[sortBy].localeCompare(game2[sortBy]);
+      return game1.name.localeCompare(game2.name);
     } else if (sortBy === "createdAt") {
-      return game2[sortBy] - game1[sortBy];
+      return game2.id - game1.id;
     }
   });
 
@@ -52,16 +82,37 @@ export default function Games() {
         />
       </div>
       <section className="filters-sorting">
+        <div>
+          <div className="filter" onClick={handleDorpdown}>
+            <img src="src\assets\Icons\fi-rr-filter.svg" alt="" />
+            <h3>Filter</h3>
+          </div>
+          <div
+            className="dropdown"
+            style={{ display: dropdown ? "block" : "none" }}
+          >
+            {allTags.map((tag) => (
+              <label key={tag}>
+                <input
+                  type="checkbox"
+                  value={tag}
+                  checked={selectedTags.includes(tag)}
+                  onChange={handleTagSelection}
+                />
+                {tag}
+              </label>
+            ))}
+          </div>
+        </div>
         <label>
           <img src="src\assets\Icons\sortingArrows.svg" alt="" />
-
           <select onChange={(e) => setSortBy(e.target.value)}>
             <option value="createdAt">Newest</option>
             <option value="name">Name</option>
           </select>
         </label>
       </section>
-      <section className="allGames ">
+      <section className="allGames">
         {gamesToDisplay.map((game) => (
           <GameCard game={game} key={game.id} />
         ))}
