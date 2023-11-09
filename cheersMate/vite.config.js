b6cf4e775d7eb656aa13adad10bcb19d6ed1dcd3
maja <eaamkly@students.eaaa.dk>
2023-11-09 17:10:09 +0100

    aa

--- cheersMate/vite.config.js
+++ cheersMate/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
@@ -9,7 +9,7 @@ export default defineConfig(({ command }) => {
   };
 
   if (command !== "serve") {
-    config.base = "/webbApp-cheersMate-/";
+    config.base = "/cheersMate/";
   }
 
   return config;
