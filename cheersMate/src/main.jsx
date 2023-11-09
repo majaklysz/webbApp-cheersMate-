c81308c5a438097ebc68bbfe58bd516e69a5dfc4
maja <eaamkly@students.eaaa.dk>
2023-11-09 17:27:29 +0100

    aaa

--- cheersMate/src/main.jsx
+++ cheersMate/src/main.jsx
@@ -6,9 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter
-      basename={import.meta.env.DEV ? "/" : "/webbApp-cheersMate-/"}
-    >
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
