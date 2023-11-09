5906d726a870d33e953c44501b73583968b0b74f
maja <eaamkly@students.eaaa.dk>
2023-11-09 17:19:35 +0100

    lala

--- cheersMate/vite.config.js
+++ cheersMate/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ command }) => {
   };
 
   if (command !== "serve") {
-    config.base = "/cheersMate/";
+    config.base = "/webbApp-cheersMate-/";
   }
 
   return config;
