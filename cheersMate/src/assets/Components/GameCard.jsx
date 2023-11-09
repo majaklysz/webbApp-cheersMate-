e31457d76241453e1ea12ea5cad783f3ac154e62
maja <eaamkly@students.eaaa.dk>
2023-11-09 14:10:23 +0100

    Update GameCard.jsx

--- cheersMate/src/assets/Components/GameCard.jsx
+++ cheersMate/src/assets/Components/GameCard.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
 export default function GameCard({ game }) {
   const localStorageKey = `liked_${game.id}`;
   const [isLiked, setIsLiked] = useState(
     localStorage.getItem(localStorageKey) === "true"
   );
-
+  const navigate = useNavigate();
   const toggleLike = () => {
     const newLikedState = !isLiked;
     setIsLiked(newLikedState);
@@ -23,7 +24,12 @@ export default function GameCard({ game }) {
     : "/src/assets/Icons/blackIcons/png/heartblackEmpty.png";
   return (
     <article className="mainCard" key={game.id}>
-      <img className="gameImage" src={game.imageURL} alt={game.name} />
+      <img
+        onClick={() => navigate(`games/${game.id}`)}
+        className="gameImage"
+        src={game.imageURL}
+        alt={game.name}
+      />
       <div className="cardSmallDescription">
         <div className="players">
           <img src="src\assets\Icons\blackIcons\playersIcon.png" alt="" />
